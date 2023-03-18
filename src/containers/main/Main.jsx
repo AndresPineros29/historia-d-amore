e8fa0bc3fdab66af90bc85397ba44936f7fa1a62
AndresPineros29 <andespipa29@gmail.com>
2023-03-17 19:08:02 -0500

    router fix

--- src/containers/main/Main.jsx
+++ src/containers/main/Main.jsx
@@ -2,8 +2,9 @@ import { Home, ReservationPage, Contact } from "../../pages";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 const Main = () => {
+  const location = useLocation();
   return (
-    <Routes>
+    <Routes key={location.pathname} location={location}>
       <Route path="/" element={<Home />} />
       <Route path="/reservation" element={<ReservationPage />} />
     </Routes>
